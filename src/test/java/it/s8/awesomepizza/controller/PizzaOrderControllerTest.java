9ad64f9f5977dc3000088366de42b4a776a79984
francesco-s8 <francesco.sigolotto@gmail.com>
2026-06-14 13:19:09 +0200

    Add simple controller test

--- src/test/java/it/s8/awesomepizza/controller/PizzaOrderControllerTest.java
+++ src/test/java/it/s8/awesomepizza/controller/PizzaOrderControllerTest.java
@@ -1,11 +1,55 @@
 package it.s8.awesomepizza.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import it.s8.awesomepizza.exception.PizzaOrderAlreadyDeliveredException;
+import it.s8.awesomepizza.service.impl.PizzaOrderFacadeImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(controllers = PizzaOrderController.class)
 class PizzaOrderControllerTest {
 
+  @Autowired private MockMvc mockMvc;
 
+  @MockitoBean PizzaOrderFacadeImpl pizzaOrderFacade;
+
+  @Test
+  void givenAValidOrderIdShouldReturnTheCurrentOrderStatus() throws Exception {
+
+    when(pizzaOrderFacade.retrieveOrderStatus(anyLong())).thenReturn("IN_PROCESS");
+
+    var actual =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.get("/api/awesome-pizza/order/1")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    assertThat(actual.getResponse().getContentAsString()).isEqualTo("IN_PROCESS");
+  }
+
+  @Test
+  void givenAnInvalidOrderIdShouldThrowAPizzaOrderAlreadyDeliveredException() throws Exception {
+
+    when(pizzaOrderFacade.retrieveOrderStatus(anyLong()))
+        .thenThrow(new PizzaOrderAlreadyDeliveredException("Order 1 is already delivered"));
+
+    var actual =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.get("/api/awesome-pizza/order/1")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    assertThat(actual.getResponse().getContentAsString()).isEqualTo("Order 1 is already delivered");
+  }
 }
