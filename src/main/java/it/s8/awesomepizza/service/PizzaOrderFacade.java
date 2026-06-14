68fd842edc20558e4493af96bbb41c992388774b
francesco-s8 <francesco.sigolotto@gmail.com>
2026-06-14 16:01:10 +0200

    Fix controller tests

--- src/main/java/it/s8/awesomepizza/service/PizzaOrderFacade.java
+++ src/main/java/it/s8/awesomepizza/service/PizzaOrderFacade.java
@@ -2,10 +2,13 @@ package it.s8.awesomepizza.service;
 
 import it.s8.awesomepizza.dto.OrderDto;
 import it.s8.awesomepizza.dto.OrderRequest;
+import it.s8.awesomepizza.exception.PizzaOrderAlreadyDeliveredException;
+import jakarta.persistence.EntityNotFoundException;
 
 public interface PizzaOrderFacade {
 
   OrderDto processOrder(OrderRequest orderRequest);
 
-  String retrieveOrderStatus(Long orderId);
+  String retrieveOrderStatus(Long orderId)
+      throws EntityNotFoundException, PizzaOrderAlreadyDeliveredException;
 }
--- src/test/java/it/s8/awesomepizza/controller/PizzaOrderControllerTest.java
+++ src/test/java/it/s8/awesomepizza/controller/PizzaOrderControllerTest.java
@@ -1,13 +1,13 @@
 package it.s8.awesomepizza.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import it.s8.awesomepizza.exception.PizzaOrderAlreadyDeliveredException;
 import it.s8.awesomepizza.service.impl.PizzaOrderFacadeImpl;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -44,8 +44,25 @@ class PizzaOrderControllerTest {
     when(pizzaOrderFacade.retrieveOrderStatus(anyLong()))
         .thenThrow(new PizzaOrderAlreadyDeliveredException("Order 1 is already delivered"));
 
-    assertThatThrownBy(() -> pizzaOrderFacade.retrieveOrderStatus(1L))
-        .isInstanceOf(PizzaOrderAlreadyDeliveredException.class)
-        .hasMessage("Order 1 is already delivered");
+    var actual =
+        mockMvc
+            .perform(
+                MockMvcRequestBuilders.get("/api/awesome-pizza/order/1")
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andReturn();
+    assertThat(actual.getResponse().getContentAsString()).isEqualTo("Order 1 is already delivered");
+  }
+
+  @Test
+  void givenAnInvalidOrderIdShouldThrowAnEntityNotFoundException() throws Exception {
+
+    when(pizzaOrderFacade.retrieveOrderStatus(20L))
+        .thenThrow(new EntityNotFoundException("Order 20 not found"));
+    mockMvc
+        .perform(
+            MockMvcRequestBuilders.get("/api/awesome-pizza/order/20")
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
   }
 }
