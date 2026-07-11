f07b8974b1166108d0bac2d85ed45dc9cf80a907
francesco-s8 <francesco.sigolotto@gmail.com>
2026-07-12 01:27:12 +0200

    Remove duplicate annotation

--- src/main/java/it/s8/awesomepizza/AwesomepizzaApplication.java
+++ src/main/java/it/s8/awesomepizza/AwesomepizzaApplication.java
@@ -1,14 +1,12 @@
 package it.s8.awesomepizza;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableRabbit
 @SpringBootApplication
 public class AwesomepizzaApplication {
 
-    static void main(String[] args) {
-        SpringApplication.run(AwesomepizzaApplication.class, args);
-    }
+  static void main(String[] args) {
+    SpringApplication.run(AwesomepizzaApplication.class, args);
+  }
 }
