e19f8a15b6806b8b5d1ea382f5735479375575fd
francesco-s8 <francesco.sigolotto@gmail.com>
2026-09-18 22:15:33 +0200

    Add constraint

--- src/main/java/it/s8/awesomepizza/dto/OrderDto.java
+++ src/main/java/it/s8/awesomepizza/dto/OrderDto.java
@@ -1,4 +1,12 @@
 package it.s8.awesomepizza.dto;
 
 public record OrderDto(long orderId) {
+
+  @Override
+  public long orderId() {
+    if (orderId < 0) {
+      throw new IllegalArgumentException("Param orderId must be positive");
+    }
+    return orderId;
+  }
 }
